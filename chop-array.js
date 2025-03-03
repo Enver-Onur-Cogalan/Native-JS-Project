17c899995d57df1e571ebe43fdf1d7b05750f9b6
Enver Onur Çoğalan <ericmoren@Enver-MacBook-Air.local>
2025-03-03 13:28:39 +0300

    :star: Added solution for chop-array.js

--- chop-array.js
+++ chop-array.js
@@ -21,6 +21,19 @@
 function chopArray(array, numElements) {
     // TODO: Implement Me
     const result = {};
+    let groupNum = 1; // To keep track of group numbers
+
+    // Split the array by the specified number of elements
+    for (let i = 0; i < array.length; i += numElements) {
+        let group = array.slice(i, i + numElements); // Shredding process
+
+        if (group.length === numElements) {
+            result[`group${groupNum}`] = group;
+            groupNum++; //Increment counter for next group
+        } else {
+            result['remaining'] = group; // // If the last group is not completed, add it as "remainder"
+        }
+    }
 
     console.log(result);
     return result;
@@ -29,7 +42,7 @@ function chopArray(array, numElements) {
 // Kodunuzu farkli girdilerle test edebilirsiniz.
 // Dosyayi calistirmak icin, Terminal >> node chop-array.js
 chopArray(['a', 'b', 1, 2, 'c', 'd', 525], 4);
-chopArray([10,20,11,30,41,50], 3);
-chopArray([10,20,11,30,41,50], 2);
-chopArray([10,20,11,30,41,50], 1);
-chopArray([10,20,11,30,41,50], 6);
+chopArray([10, 20, 11, 30, 41, 50], 3);
+chopArray([10, 20, 11, 30, 41, 50], 2);
+chopArray([10, 20, 11, 30, 41, 50], 1);
+chopArray([10, 20, 11, 30, 41, 50], 6);
